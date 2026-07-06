c50a865bfe8a0b0479cf6456a7c0114dc8738fb4
JeiKarousel <theconfid3ntialz@gmail.com>
2026-07-06 10:56:48 +0800

    some that didnt commit for reason

--- Assignment/src/User/UserDB.java
+++ Assignment/src/User/UserDB.java
@@ -1,6 +1,23 @@
 package User;
+import java.util.ArrayList;
+import java.util.List;
 
-
+//implemented singleton design pattern
 public class UserDB {
     
+    private static UserDB userDB = new UserDB();
+    private UserDB(){};
+    
+    public static UserDB getInstance(){
+        return userDB;
+    }
+    
+    public void addUser(User user){};
+    public void loadUsers(User user){};
+    public void saveUser(User user){};
+    public void deleteUser(User user){};
+    public void updateUser(User user){};
+    public void findUser(User user){};
+    public void getAllUsers(User user){};
+
 }
