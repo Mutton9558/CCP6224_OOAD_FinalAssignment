0379808911f188a7415aa8199c521466596ee7da
Shawn Huang Qi Yang <htp8090@gmail.com>
2026-07-08 18:04:12 +0800

    Sync the db informations across modules

--- Assignment/src/Rental/RentalDB.java
+++ Assignment/src/Rental/RentalDB.java
@@ -21,8 +21,8 @@ public class RentalDB {
             String insertQuery = "INSERT OR IGNORE INTO Rentals (user_id, equipment, bookedDate, duration, returnStatus, lateStatus) VALUES (?, ?, ?, ?, ?, ?)";
     
             Object[][] testData = {
-                {1, 1, LocalDate.now(), 2, false, false},
-                {1, 2, LocalDate.now(), 8, false, false},
+                {1, 1, LocalDate.now(), 2, true, false},
+                {1, 2, LocalDate.now(), 8, true, false},
                 {3, 3, LocalDate.now(), 4, false, false}
             };
     
